6d69242bd2daa91d7231341d67f5721df40481a0
LenOk <95621680+wee-owl@users.noreply.github.com>
2022-07-29 10:51:49 +0300

    feat: change redirect uri

--- scripts/const.js
+++ scripts/const.js
@@ -4,7 +4,6 @@ export const API_URL_TOKEN = 'https://unsplash.com/oauth/token';
 export const API_URL_AUTH = 'https://unsplash.com/oauth/authorize';
 export const ACCESS_KEY = 'sXHCp2opUUgVl4NBR9eeuijO4VJjqYQCTjsfKIX4mvs';
 export const SECRET_KEY = '-TRW9mqWITUDjC9gYct2Ytkf5vdN9S8Rath4NyI-Dy8';
-export const REDIRECT_URI = 'http://127.0.0.1:5500/';
+export const REDIRECT_URI = 'https://photo-stream-wee-owl.vercel.app/';
 export const RESPONSE_TYPE = 'code';
 export const SCOPE = 'public read_user read_photos write_likes';
-
